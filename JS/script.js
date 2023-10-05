64d8494339039c5a903fe710ebc888372be0f8c5
Khelbot <khelsea_alarefi@fitnyc.edu>
2023-10-04 22:43:56 -0400

    Created JS date/time for displayed cities

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  //Milan
+  let milanElement = document.querySelector("#milan");
+  let milanDateElement = milanElement.querySelector(".date");
+  let milanTimeElement = milanElement.querySelector(".time");
 
+  let milanTime = moment().tz("Europe/Vatican");
+  milanDateElement.innerHTML = milanTime.format("MMMM do, YYYY");
+  milanTimeElement.innerHTML = milanTime.format(
+    "h:mm:ss[<small>]:sss A[</small>]"
+  );
+
+  //Accra
+  let accraElement = document.querySelector("#accra");
+  let accraDateElement = accraElement.querySelector(".date");
+  let accraTimeElement = accraElement.querySelector(".time");
+
+  let accraTime = moment().tz("Africa/Accra");
+  accraDateElement.innerHTML = accraTime.format("MMMM do, YYYY");
+  accraTimeElement.innerHTML = accraTime.format(
+    "h:mm:ss[<small>]:sss A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
