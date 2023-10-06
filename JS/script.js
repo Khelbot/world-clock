45cda88af68e229ba5fd4d405161f0e0c5551af7
Khelbot <khelsea_alarefi@fitnyc.edu>
2023-10-05 22:54:04 -0400

    Added Location Functionality

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,48 @@
 function updateTime() {
   //Milan
+
   let milanElement = document.querySelector("#milan");
   let milanDateElement = milanElement.querySelector(".date");
   let milanTimeElement = milanElement.querySelector(".time");
 
   let milanTime = moment().tz("Europe/Vatican");
   milanDateElement.innerHTML = milanTime.format("MMMM do, YYYY");
-  milanTimeElement.innerHTML = milanTime.format(
-    "h:mm:ss[<small>]:sss A[</small>]"
-  );
+  milanTimeElement.innerHTML = milanTime.format("h:mm:ss[<small>]A[</small>]");
 
   //Accra
+
   let accraElement = document.querySelector("#accra");
   let accraDateElement = accraElement.querySelector(".date");
   let accraTimeElement = accraElement.querySelector(".time");
 
   let accraTime = moment().tz("Africa/Accra");
   accraDateElement.innerHTML = accraTime.format("MMMM do, YYYY");
-  accraTimeElement.innerHTML = accraTime.format(
-    "h:mm:ss[<small>]:sss A[</small>]"
-  );
+  accraTimeElement.innerHTML = accraTime.format("h:mm:ss[<small>] A[</small>]");
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM do, YYYY")}</div>
+      </div>
+      <div>
+        <div class="time">
+          ${cityTime.format("h:mm:ss")}
+          <small>${cityTime.format("A")}</small>
+        </div>
+      </div>
+    </div>
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
